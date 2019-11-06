016c5539ebbb03799bd42a7e94da29ab97dabc16
Kink <super_wyx@163.com>
2019-11-06 09:30:38 +0800

    使用HostService创建的控制台宿主，更新说明文档

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Akanami.Framework
+
+# 总结.Net Core程序开发的相关样例程序
+
+## 宿主启动程序
+
+### 控制台启动的宿主程序
+
+使用微软自带的库，通过控制台作为宿主程序启动。该启动程序与Asp.Net Core的启动程序风格一致。
+具体代码请参考 [源代码中netcore/Akanami.Host/Akanami.Host.sln解决方案中的ConsoleHost工程](https://github.com/Kink007/Akanami.Framework/tree/initialize/netcore/Akanami.Host)
--- netcore/Akanami.Host/ConsoleHost/Program.cs
+++ netcore/Akanami.Host/ConsoleHost/Program.cs
@@ -22,21 +22,25 @@ namespace ConsoleHost
         {
             HostBuilder hostBuilder = new HostBuilder();
 
+            //注册Logger驱动程序
             hostBuilder.ConfigureLogging((logging) =>
             {
                 logging.AddConsole();
             });
 
+            //注册配置文件
             hostBuilder.ConfigureAppConfiguration((configurationBuilder) =>
             {
                 var builder = configurationBuilder.AddJsonFile("appSettings.json");
             });
 
+            //注册相关的服务，以及进行DependencyInjection
             hostBuilder.ConfigureServices((serviceCollection) =>
             {
                 serviceCollection.AddSingleton<IHostedService, HostService>();
             });
 
+            //处理完上述程序后即可启动
             await hostBuilder.RunConsoleAsync();
         }
     }
