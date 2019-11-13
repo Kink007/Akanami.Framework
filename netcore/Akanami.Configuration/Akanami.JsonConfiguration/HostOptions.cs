4440b090e752e71f0d5651b74c1335f8f41def1b
Kink <super_wyx@163.com>
2019-11-13 17:42:21 +0800

    提交配置相关

---
+++ netcore/Akanami.Configuration/Akanami.JsonConfiguration/HostOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akanami.JsonConfiguration
+{
+    public class HostOptions
+    {
+        public string Name { get; set; }
+
+        public string TestValue { get; set; }
+    }
+}
---
+++ netcore/Akanami.Configuration/Akanami.JsonConfiguration/HostService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Akanami.JsonConfiguration
+{
+    public class HostService : IHostedService
+    {
+        private readonly HostOptions options;
+        private readonly ILogger logger;
+
+        public HostService(IOptions<HostOptions> options, ILogger<HostService> logger)
+        {
+            this.options = options.Value;
+            this.logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("==============================================================");
+            this.logger.LogInformation($"Options Name:{this.options.Name} TestValue:{this.options.TestValue}");
+            this.logger.LogInformation("==============================================================");
+
+            await Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("停止服务");
+
+            await Task.CompletedTask;
+        }
+    }
+}
